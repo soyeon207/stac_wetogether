0f7e063df6c9fcec0edf4e98a56d08867579a9f1
soyeon207 <psy010207@naver.com>
2019-09-02 13:58:35 +0900

    [Add] Firebase Auth 확인과 방을 만들었는지 확인하는 Splash 추가

--- app/src/main/java/com/example/we_together/Splash.java
+++ app/src/main/java/com/example/we_together/Splash.java
@@ -2,6 +2,7 @@ package com.example.we_together;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,11 +18,15 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         firebaseAuth = firebaseAuth.getInstance();
-        if(firebaseAuth.getCurrentUser()!=null){
+        if(firebaseAuth.getCurrentUser()!=null){ // 만약 로그인을 했다면
 
-
-            Intent intent = new Intent(Splash.this,MakeRoom.class);
+            Intent intent = new Intent(Splash.this,MainActivity.class); //메인 화면으로 넘겨주고
             startActivity(intent);
+            finish();
+        }
+
+        else { //로그인을 하지 않았따면
+            startActivity(new Intent(Splash.this,Login.class)); // 로그인 화면으로 넘겨준다
             finish();
         }
 
