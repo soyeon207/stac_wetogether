3549246e86250d5d5ceb63076d81c5e915310faf
GsDaisy <42744032+GsDaisy@users.noreply.github.com>
2019-09-09 08:44:59 +0900

    [suyoung] : placeActivity 합치기

--- app/src/main/java/com/example/we_together/placeActivity.java
+++ app/src/main/java/com/example/we_together/placeActivity.java
@@ -1,16 +1,25 @@
 package com.example.we_together;
 
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
@@ -26,11 +35,12 @@ public class placeActivity extends AppCompatActivity /*implements CompoundButton
     private CheckBox ch9;
     private CheckBox ch10;
     private Button btn;
-    int cnt=0;
+    private Button goStart;
+    int cnt;
     LinearLayout linearLayout;
     public ArrayList<String> placeList = new ArrayList<>();
     Context context;
-    Button go;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,25 +58,20 @@ public class placeActivity extends AppCompatActivity /*implements CompoundButton
         ch9 = (CheckBox)findViewById(R.id.checkBox10);
         ch10 = (CheckBox)findViewById(R.id.checkBox11);
         btn = findViewById(R.id.addButton);
+        cnt=0;
+        goStart = findViewById(R.id.goStart);
         context = this;
-        go = findViewById(R.id.goStart);
-        /*ch1.setOnCheckedChangeListener(this);
-        ch2.setOnCheckedChangeListener(this);
-        ch3.setOnCheckedChangeListener(this);
-        ch4.setOnCheckedChangeListener(this);
-        ch5.setOnCheckedChangeListener(this);
-        ch6.setOnCheckedChangeListener(this);
-        ch7.setOnCheckedChangeListener(this);
-        ch8.setOnCheckedChangeListener(this);
-        ch9.setOnCheckedChangeListener(this);
-        ch10.setOnCheckedChangeListener(this);*/
+       /*ch1.setOnCheckedChangeListener(this);
+       ch2.setOnCheckedChangeListener(this);
+       ch3.setOnCheckedChangeListener(this);
+       ch4.setOnCheckedChangeListener(this);
+       ch5.setOnCheckedChangeListener(this);
+       ch6.setOnCheckedChangeListener(this);
+       ch7.setOnCheckedChangeListener(this);
+       ch8.setOnCheckedChangeListener(this);
+       ch9.setOnCheckedChangeListener(this);
+       ch10.setOnCheckedChangeListener(this);*/
 
-        go.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(placeActivity.this,MainActivity.class));
-            }
-        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,11 +128,11 @@ public class placeActivity extends AppCompatActivity /*implements CompoundButton
                     }
                 }
                 Button btn[] = new Button[placeList.size()];
-                int i=0;
+                if(cnt==0) {
+                    int i=0;
+                    for(String place : placeList){
 
-                for(String place : placeList){
 
-                    if(cnt==0) {
                         //for(int i=0;i<btn.length;i++) {
                         //if(placeList.contains(place))
                         btn[i] = new Button(context);
@@ -137,15 +142,23 @@ public class placeActivity extends AppCompatActivity /*implements CompoundButton
                         btn[i].setId(i);
                         linearLayout.addView(btn[i]);
 
-/*                        btn[i].setOnClickListener(new View.OnClickListener() {
+
+                        btn[i].setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
 
                             }
-                        });*/
+                        });
                         i++;
-                    }else{
-                        linearLayout.removeAllViewsInLayout();
+                    }
+                    cnt=1;
+                }else{
+                    int i=0;
+                    linearLayout.removeAllViewsInLayout();
+                    for(String place : placeList){
+
+
+
                         btn[i] = new Button(context);
                         btn[i].setText(place);
                         btn[i].setWidth(66);
@@ -153,79 +166,167 @@ public class placeActivity extends AppCompatActivity /*implements CompoundButton
                         btn[i].setId(i);
                         linearLayout.addView(btn[i]);
 
-/*                        btn[i].setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
 
-                            }
-                        });*/
                         i++;
                     }
-                    //}
+
+
+
                 }
+
+/*                for(String place : placeList){
+
+                   if(cnt==0) {
+                       //for(int i=0;i<btn.length;i++) {
+                       //if(placeList.contains(place))
+                       btn[i] = new Button(context);
+                       btn[i].setText(place);
+                       btn[i].setWidth(66);
+                       btn[i].setTextSize(11);
+                       btn[i].setId(i);
+                       linearLayout.addView(btn[i]);
+
+
+*//*                        btn[i].setOnClickListener(new View.OnClickListener() {
+                           @Override
+                           public void onClick(View view) {
+
+                           }
+                       });*//*
+                       i++;
+                   }else{
+                       linearLayout.removeAllViewsInLayout();
+                       btn[i] = new Button(context);
+                       btn[i].setText(place);
+                       btn[i].setWidth(66);
+                       btn[i].setTextSize(11);
+                       btn[i].setId(i);
+                       linearLayout.addView(btn[i]);
+
+*//*                        btn[i].setOnClickListener(new View.OnClickListener() {
+                           @Override
+                           public void onClick(View view) {
+
+                           }
+                       });*//*
+                       i++;
+                   }
+                   //}
+               }
+               cnt=1;*/
 
             }
         });
 
 
-    }
-    /*@Override
-    public void onCheckedChanged(CompoundButton button, boolean isChecked){
+        goStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setStringArrayPref(context, "pp" , placeList);
 
-        if(ch1.isChecked()){
-            placeList.add((String)ch1.getText());
-        }
-        if(ch2.isChecked()){
-            placeList.add((String)ch2.getText());
-        }
-        if(ch3.isChecked()){
-            placeList.add((String)ch3.getText());
-        }
-        if(ch4.isChecked()){
-            placeList.add((String)ch4.getText());
-        }
-        if(ch5.isChecked()){
-            placeList.add((String)ch5.getText());
-        }
-        if(ch6.isChecked()){
-            placeList.add((String)ch6.getText());
-        }
-        if(ch7.isChecked()){
-            placeList.add((String)ch7.getText());
-        }
-        if(ch8.isChecked()){
-            placeList.add((String)ch8.getText());
-        }
-        if(ch9.isChecked()){
-            placeList.add((String)ch9.getText());
-        }
-        if(ch10.isChecked()){
-            placeList.add((String)ch10.getText());
-        }
-        for(String place : placeList){
-
-            Button btn[] = new Button[placeList.size()];
-            for(int i=0;i<btn.length;i++) {
-                btn[i] = new Button(this);
-                btn[i].setText(place);
-                btn[i].setWidth(66);
-                btn[i].setTextSize(11);
-                btn[i].setId(i);
-                linearLayout.addView(btn[i]);
-
-                btn[i].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
+                ArrayList<String> list = getStringArrayPref(context,"pp");
+                if (list != null) {
+                    for (String value : list) {
+                        Log.d("ㅇㅇㅇㅇ","Get json : " + value);
                     }
-                });
+                }
 
+                startActivity(new Intent(placeActivity.this,MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+
+    private void setStringArrayPref(Context context, String key, ArrayList<String> values){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.apply();
+    }
+    private ArrayList<String> getStringArrayPref(Context context, String key) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
-
+        return urls;
     }
+   /*@Override
+   public void onCheckedChanged(CompoundButton button, boolean isChecked){
+
+       if(ch1.isChecked()){
+           placeList.add((String)ch1.getText());
+       }
+       if(ch2.isChecked()){
+           placeList.add((String)ch2.getText());
+       }
+       if(ch3.isChecked()){
+           placeList.add((String)ch3.getText());
+       }
+       if(ch4.isChecked()){
+           placeList.add((String)ch4.getText());
+       }
+       if(ch5.isChecked()){
+           placeList.add((String)ch5.getText());
+       }
+       if(ch6.isChecked()){
+           placeList.add((String)ch6.getText());
+       }
+       if(ch7.isChecked()){
+           placeList.add((String)ch7.getText());
+       }
+       if(ch8.isChecked()){
+           placeList.add((String)ch8.getText());
+       }
+       if(ch9.isChecked()){
+           placeList.add((String)ch9.getText());
+       }
+       if(ch10.isChecked()){
+           placeList.add((String)ch10.getText());
+       }
+       for(String place : placeList){
+
+           Button btn[] = new Button[placeList.size()];
+           for(int i=0;i<btn.length;i++) {
+               btn[i] = new Button(this);
+               btn[i].setText(place);
+               btn[i].setWidth(66);
+               btn[i].setTextSize(11);
+               btn[i].setId(i);
+               linearLayout.addView(btn[i]);
+
+               btn[i].setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+
+                   }
+               });
+
+           }
+       }
+
+   }
 */
 
 
 
 }
+
