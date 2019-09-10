3e441ff1219a88127193f4855cd0bad06a40e5bf
soyeon207 <psy010207@naver.com>
2019-09-10 14:36:19 +0900

    [Add] #5 dialog 띄우기 완성

--- app/src/main/java/com/example/we_together/inviteDialog.java
+++ app/src/main/java/com/example/we_together/inviteDialog.java
@@ -1,15 +1,38 @@
 package com.example.we_together;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class inviteDialog extends AppCompatActivity {
+public class inviteDialog extends Dialog {
+
+    private Button exit_btn;
+    private View.OnClickListener exit_listner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //다이얼로그 밖의 화면은 흐리게 만들어줌
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount = 0.8f;
+        getWindow().setAttributes(layoutParams);
+
         setContentView(R.layout.activity_invite_dialog);
 
+        exit_btn = findViewById(R.id.dialog_exit);
+        exit_btn.setOnClickListener(exit_listner);
 
+    }
+
+    public inviteDialog(@NonNull Context context, View.OnClickListener exit_listner){
+        super(context);
+        this.exit_listner = exit_listner;
     }
 }
