9f07275ab73e6079f198ec1e724a54c3dda68113
soyeon207 <psy010207@naver.com>
2019-09-10 13:40:51 +0900

    [Add] inviteDialog 추가

---
+++ app/src/main/java/com/example/we_together/inviteDialog.java
@@ -0,0 +1,13 @@
+package com.example.we_together;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inviteDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_dialog);
+    }
+}
