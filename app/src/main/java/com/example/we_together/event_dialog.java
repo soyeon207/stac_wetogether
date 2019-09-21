b7171f655a511c6a8204dbfe1713db1232ca1466
soyeon207 <psy010207@naver.com>
2019-09-22 04:21:45 +0900

    [Add] #7 달력 dialog 추가

---
+++ app/src/main/java/com/example/we_together/event_dialog.java
@@ -0,0 +1,20 @@
+package com.example.we_together;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class event_dialog extends Dialog {
+
+    public event_dialog(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_dialog);
+    }
+}
